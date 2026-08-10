a5a9c66d1920ad2b315b6a169dc9d2afd334f35f
Gabriel Labritz <gabriellabritz@gmail.com>
2026-08-10 12:21:32 -0300

    feat: Criando a entidade email verfication token

---
+++ src/main/java/com/gabriellabritz/build_finance_api/domain/authentication/email_verificaton_tokens/EmailVerificationToken.java
@@ -0,0 +1,29 @@
+package com.gabriellabritz.build_finance_api.domain.authentication.email_verificaton_tokens;
+
+import com.gabriellabritz.build_finance_api.domain.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "email_verification_tokens")
+@Getter
+@NoArgsConstructor
+public class EmailVerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "token_hash", nullable = false, unique = true, columnDefinition = "BINARY(32)")
+    private byte[] tokenHash;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+}
