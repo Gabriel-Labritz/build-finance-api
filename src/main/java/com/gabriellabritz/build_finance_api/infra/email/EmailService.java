d80cbc5c33a310bf03dcc6a86078275334d24d40
Gabriel Labritz <gabriellabritz@gmail.com>
2026-08-19 13:53:13 -0300

    feat: Criando o template HTML para verificação da conta por email

--- src/main/java/com/gabriellabritz/build_finance_api/infra/email/EmailService.java
+++ src/main/java/com/gabriellabritz/build_finance_api/infra/email/EmailService.java
@@ -3,12 +3,15 @@ package com.gabriellabritz.build_finance_api.infra.email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class EmailService {
@@ -22,6 +25,9 @@ public class EmailService {
 
     @Value("${app.url.site}")
     private String urlSite;
+
+    @Value("classpath:templates/email-verification-template.html")
+    private Resource emailVerificationTemplate;
 
     public EmailService(JavaMailSender javaMailSender) {
         this.javaMailSender = javaMailSender;
@@ -48,8 +54,18 @@ public class EmailService {
         String verificationUrl = urlSite.concat("/account/verify-account?token=").concat(token);
         String subject = "Build Finance - Verifique sua conta";
 
-        String bodyEmail = "Verifique sua conta";
+        String bodyEmail = loadTemplate(emailVerificationTemplate)
+                .replace("{userName}", userName)
+                .replace("{verificationUrl}", verificationUrl);
 
         sendEmail(userEmail, subject, bodyEmail);
+    }
+
+    private String loadTemplate(Resource resource) {
+        try {
+            return resource.getContentAsString(StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao carregar template", e);
+        }
     }
 }
