dc5c8ff15591e3fa9e0fe07ac2a66eb0f0377c3b
Gabriel Labritz <gabriellabritz@gmail.com>
2026-09-10 13:16:17 -0300

    feat: Implementando o access token filter

---
+++ src/main/java/com/gabriellabritz/build_finance_api/infra/filters/AccessTokenFilter.java
@@ -0,0 +1,58 @@
+package com.gabriellabritz.build_finance_api.infra.filters;
+
+import com.gabriellabritz.build_finance_api.domain.user.User;
+import com.gabriellabritz.build_finance_api.domain.user.UserRepository;
+import com.gabriellabritz.build_finance_api.infra.jwt.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class AccessTokenFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    public AccessTokenFilter(JwtService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = getTokenFromHeader(request);
+
+        if(token != null) {
+            String userIdSubject = jwtService.getSubjectFromToken(token);
+
+            UUID userId = UUID.fromString(userIdSubject);
+
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new UsernameNotFoundException("O usuário não foi encontrado."));
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getTokenFromHeader(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7);
+        }
+
+        return null;
+    }
+}
